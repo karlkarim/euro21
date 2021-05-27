411034d9df6518b20fd2495e570cd132868f52ce
Rain <rkullamaa@gmail.com>
2021-05-27 17:06:47 +0300

    :sparkles: single match comp :sparkles:

--- euro21/src/Components/Wrapper/index.js
+++ euro21/src/Components/Wrapper/index.js
@@ -1,6 +1,6 @@
 const Wrapper = ({ children }) => {
   return ( 
-    <div className="container px-6 mx-auto mt-24 mb-24 md:max-w-6xl text-darkBlue">
+    <div className="container px-2 mx-auto mt-24 mb-24 md:max-w-6xl text-darkBlue">
       {children}
     </div>
    );
--- euro21/src/Pages/App/index.js
+++ euro21/src/Pages/App/index.js
@@ -1,7 +1,26 @@
+import Flag from 'react-world-flags'
 const MainApp = () => {
   return ( 
-    <div>
-      Sisse logitud - loo uus ennustus mäng
+    <div className='grid gap-4'>
+      <div className='grid items-center grid-cols-3 p-4 bg-white border border-gray-100 rounded-md shadow-md select-none'>
+        <div className='flex flex-col items-center'>
+          <Flag className='inline object-cover w-10 h-10 mr-2 border border-gray-100 rounded-full shadow-md' code="cze" fallback={ <span>Unknown</span> }/>
+          <div className='text-center'>Cezh Republic</div>
+        </div>
+        <div>
+          <div className='flex flex-col items-center'>
+            <div className='text-xs uppercase'>Starting date</div>
+            <div className='text-xs'>01/03/2918 | 21:11</div>
+            <div
+              onClick={() => alert('Siia ennustus asi')}
+              className='mt-2 text-3xl'>3 : 2</div>
+          </div>
+        </div>
+        <div className='flex flex-col items-center'>
+        <Flag className='inline object-cover w-10 h-10 mr-2 border border-gray-100 rounded-full shadow-md' code="mkd" fallback={ <span>Unknown</span> }/>
+          <div className='text-center'>North Macedonia</div>
+        </div>
+      </div>
     </div>
    );
 }
